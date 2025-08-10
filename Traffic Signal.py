718df6454a9739e27d283c40e834f0b6ad65d7c0
Yashwanth M <yashwanthm897@gmail.com>
2025-08-10 18:34:29 +0530

    Create Traffic Signal.py

---
+++ Traffic Signal.py
@@ -0,0 +1,17 @@
+#a funny way to roast yourself
+
+print("Imagine you are at a traffic signal.\nDon't know the rules? Don't worry.\nI'll teach you in a simple way")
+
+light=input("Enter what color are you seeing now\nat the traffic signal : ").lower()
+
+if(light=="red"):
+  print("Red, eh? Then you should wait.")
+  
+elif(light=="yellow"):
+  print("Yellow? You should shut the fuck up and wait for some time.")
+
+elif(light=="green"):
+  print("Green!!! Let's fucking go.\nNow you can move your ass.")
+
+else:
+  print("Traffic lights doesn't show",light,", asshole.\nDon't you know that simple thing?")
